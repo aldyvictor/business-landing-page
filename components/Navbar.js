727d57bb80be510758a41d73ff6413b699e52c41
Aldy Victor Rianes <aldyvictor1046@gmail.com>
2024-01-17 10:55:22 +0700

    setup and layouting

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flexBetween padding-container'>
+
+    </nav>
+  )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,42 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          red : {
+            100: '#F15757',
+          },
+          yellow: {
+            100: '#F88E12',
+          },
+          light: {
+            100: '#F6F6F9',
+          },
+        },
+        secondary: {
+          blue: {
+            100: '#2B71FA',
+          },
+          light: {
+            100: '#F6F6F9',
+          },
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      screens: {
+        xs: '400px',
+        '3xl': '1680px',
+        '4xl': '2200px',
+      },
+      maxWidth: {
+        '10xl': '1512px',
+      },
+      borderRadius: {
+        '5xl': '40px',
       },
     },
   },
